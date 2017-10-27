9b7a37edd0e4d3d02ca3151560327b1294748a39
lewyT <L>
2017-10-27 14:03:26 +0200

    huhuh

--- src/test/java/kolekcje/MapTest.java
+++ src/test/java/kolekcje/MapTest.java
@@ -1,7 +1,9 @@
 package kolekcje;
 
 import finanse.Kwota;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,5 +17,15 @@ public class MapTest {
         mapa.put(new Kwota(), "zero zł zero gr");
         mapa.put(new Kwota(1,20),"jeden zl dwadziescia gr");
         mapa.put(new Kwota(35,0),"trzydziesci piec zl zero gr");
+    }
+
+    @Test
+    public void znajdujeKluczWartosc(){
+        Kwota k1 = new Kwota();
+        mapa.put(k1,"zero zl zero gr");
+        Assert.assertTrue(mapa.containsValue("zero zl zero gr"));
+        Assert.assertTrue(mapa.containsKey(k1));
+
+
     }
 }
