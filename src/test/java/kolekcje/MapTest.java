02116f5d8ea0c4c9cc08692c4b7177fa2622646f
lewyT <L>
2017-10-27 14:21:41 +0200

    huhuh

--- src/test/java/kolekcje/MapTest.java
+++ src/test/java/kolekcje/MapTest.java
@@ -20,6 +20,16 @@ public class MapTest {
     }
 
     @Test
+    public void podmieniaWartosc() {
+        //given
+        //when
+       String staraWartosc =  mapa.put(new Kwota(35,0),"trzydziesci piec zl");
+        //then
+        Assert.assertEquals("trzydziesci piec zl zero gr",staraWartosc );
+        Assert.assertTrue(mapa.containsValue("trzydziesci piec zl"));
+    }
+
+    @Test
     public void znajdujeKluczWartosc(){
         Kwota k1 = new Kwota();
         mapa.put(k1,"zero zl zero gr");
@@ -28,4 +38,18 @@ public class MapTest {
 
 
     }
+
+
+
+    @Test
+    public void wartoscZaweiraKlucze(){
+        //given
+        Kwota[] oczekiwana = {new Kwota(),new Kwota(1,20),new Kwota(35,0)};
+        //then
+        Assert.assertArrayEquals(oczekiwana,mapa.keySet().toArray());
+
+    }
+
+
+
 }
